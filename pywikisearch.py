cb4df992b561696dbc9b53cf0d1cc3a305531e54
Nissaar <34180849+Nissaar@users.noreply.github.com>
2019-10-07 23:02:01 +0400

    Add files via upload

---
+++ pywikisearch.py
@@ -0,0 +1,12 @@
+import wikipedia
+
+print("Welcome to the app which find an answer to your question from wikipedia")
+
+while True:
+    ques = input("What would you like to ask Wikipedia?")
+    print wikipedia.summary(ques)
+
+
+
+
+
